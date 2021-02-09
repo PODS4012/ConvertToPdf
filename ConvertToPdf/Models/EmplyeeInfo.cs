afd9d4a3a8d7fec6ee97e4040b20ed30e2d70e12
D- CODE <darek_sokol@hotmail.com>
2021-02-09 07:46:13 +0100

    Add project files.

---
+++ ConvertToPdf/Models/EmplyeeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvertToPdf.Models
+{
+    public class EmplyeeInfo
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Department { get; set; }
+        public string Designation { get; set; }
+    }
+}
